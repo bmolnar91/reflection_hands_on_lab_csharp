7a90ea723b38582fd1089c00c5fd440da93e169b
Balint Molnar <balintkilencvenegy@gmail.com>
2020-11-15 17:36:39 +0100

    Add code for Exercise 1

--- AssemblyDemo/AssemblyDemo/Program.cs
+++ AssemblyDemo/AssemblyDemo/Program.cs
@@ -7,7 +7,31 @@ namespace AssemblyDemo
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            string path = @"C:\Windows\Microsoft.NET\Framework\v4.0.30319\System.dll";
+
+            // Load specific Assembly
+            Assembly assembly = Assembly.LoadFile(path);
+            ShowAssemblyInfo(assembly);
+
+            // Get our Assembly
+            Assembly ourAssembly = Assembly.GetExecutingAssembly();
+            ShowAssemblyInfo(ourAssembly);
+        }
+
+        static void ShowAssemblyInfo(Assembly assembly)
+        {
+            Console.WriteLine(assembly.FullName);
+            Console.WriteLine("From GAC? {0}", assembly.GlobalAssemblyCache);
+            Console.WriteLine("Path: {0}", assembly.Location);
+            Console.WriteLine("Version: {0}", assembly.ImageRuntimeVersion);
+
+            // Show Modules
+            foreach (Module module in assembly.GetModules())
+            {
+                Console.WriteLine("Mod: {0}", module.Name);
+            }
+
+            Console.WriteLine();
         }
     }
 }
