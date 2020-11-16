892adac77c229c5a8a806b3225c69cb5ae4c07e7
Balint Molnar <balintkilencvenegy@gmail.com>
2020-11-16 18:32:06 +0100

    Add code for Exercise 2

---
+++ AssemblyAttrDemo/AssemblyAttrDemo/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Reflection;
+
+namespace AssemblyAttrDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Assembly currentAssembly = Assembly.GetExecutingAssembly();
+            Type attrType = typeof(AssemblyDescriptionAttribute);
+            object[] attrs = currentAssembly.GetCustomAttributes(attrType, false);
+
+            if (attrs.Length > 0)
+            {
+                AssemblyDescriptionAttribute desc = (AssemblyDescriptionAttribute) attrs[0];
+                Console.WriteLine("Description is: {0}", desc.Description);
+            }
+        }
+    }
+}
