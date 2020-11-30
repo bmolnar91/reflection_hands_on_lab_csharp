8d673d46181759eb68de3ebb3d19d64a5f6b4826
Balint Molnar <balintkilencvenegy@gmail.com>
2020-11-30 14:53:34 +0100

    Add code for Exercise 5 (macOS)

---
+++ DemoDynamic/DemoDynamic/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Reflection;
+using System.Reflection.Emit;
+
+namespace DemoDynamic
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Create an Assembly name
+            AssemblyName theName = new AssemblyName();
+            theName.Name = "DemoAssembly";
+            theName.Version = new Version("1.0.0.0");
+
+            // Get the AppDomain to put our assembly in
+            AppDomain domain = AppDomain.CurrentDomain;
+
+            // Create the Assembly
+            AssemblyBuilder assemBldr = AssemblyBuilder.DefineDynamicAssembly(theName, AssemblyBuilderAccess.Run);
+
+            // Define a module to hold our Type
+            ModuleBuilder modBldr = assemBldr.DefineDynamicModule("DemoAssembly.dll");
+
+            // Create a new Type
+            TypeBuilder animalBldr = modBldr.DefineType("Animal", TypeAttributes.Public);
+
+            // Display new Type
+            Type animal = animalBldr.CreateType();
+            System.Console.WriteLine(animal.FullName);
+
+            foreach (MemberInfo info in animal.GetMembers())
+            {
+                System.Console.WriteLine("Member ({0}): {1}", info.MemberType, info.Name);
+            }
+        }
+    }
+}
